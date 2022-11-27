ccdd7f86ea7028e885763c0521cdd7508c3f9dda
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-27 23:30:44 +0100

    Setup routes, models and associations, and pages view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get 'pages/splash_screen'
+  devise_for :users, sign_out_via: %i[get post]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#splash_screen'
+  resources :categories, only: %i[index new create] do
+    resources :payments, only: %i[index]
+  end
+  resources :payments, only: %i[new create]
 end
+
