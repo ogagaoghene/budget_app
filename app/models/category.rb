6ebc33f13afdf81d8b9466518647d4a760286466
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-11-30 22:14:41 +0100

    Add validations to models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,9 @@
 class Category < ApplicationRecord
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :category_payments, class_name: 'CategoryPayment', foreign_key: 'category_id'
+  has_many :payments, through: :category_payments
+
+  validates :name, presence: true
+  validates :icon, presence: true
+
 end
