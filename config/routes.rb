d1ee220613378c12b9066ecb8612ce0dc574f777
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-01 00:09:25 +0100

    Add splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'pages/home'
   devise_for :users
+  root to: 'pages#home'
   resources :category_payments
   resources :payments
   resources :categories
