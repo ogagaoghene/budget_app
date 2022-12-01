47931d9a57d60241f7694b66c311c308b5efa910
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-01 01:48:08 +0100

    Update config/routes.rb file and pages_controller.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,11 @@
-# # frozen_string_literal: true
-
-# Rails.application.routes.draw do
-#   devise_for :users
-#   root to: 'pages#home'
-#   resources :category_payments
-#   resources :payments
-#   resources :categories
-#   resources :users
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-#   # Defines the root path route ("/")
-#   # root "articles#index"
-# end
+# frozen_string_literal: true
 
 Rails.application.routes.draw do
   devise_for :users, sign_out_via: %i[get post]
   # Defines the root path route ("/")
-  root to: 'pages#home'
   resources :categories, only: %i[index new create] do
     resources :payments, only: %i[index]
   end
   resources :payments, only: %i[new create]
+  root to: 'pages#home'
 end
