48c8277a76cff7a2f412657886230756ef137985
Esi Ogagaoghene <esi.ogagaoghene@yahoo.com>
2022-12-01 01:10:24 +0100

    Update devise for user authentication and setup routes for page navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,24 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
+
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root to: 'pages#home'
+#   resources :category_payments
+#   resources :payments
+#   resources :categories
+#   resources :users
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
 
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  resources :category_payments
-  resources :payments
-  resources :categories
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users, sign_out_via: %i[get post]
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: 'pages#home'
+  resources :categories, only: %i[index new create] do
+    resources :payments, only: %i[index]
+  end
+  resources :payments, only: %i[new create]
 end
